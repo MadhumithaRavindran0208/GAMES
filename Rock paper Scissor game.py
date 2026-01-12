b4c2819563d39b2f3f6ccc481500e0d6d77431d1
Madhumitha Ravindran <ammumadhu0208@gmail.com>
2026-01-13 01:03:18 +0530

    Add files via upload
    implements a Rock, Paper, Scissors game against a computer. It uses a while loop for replayability and a for loop for three-round matches. It validates user input, randomly generates computer moves, and tracks scores to declare an overall winner. Note: It contains a minor bug where n is used before being defined.

---
+++ Rock paper Scissor game.py
@@ -0,0 +1,49 @@
+print("Rock paper Scissor game")
+import random
+possibilities=["Rock"," paper ","Scissor"]
+win=loss=0
+while n!=0:
+    for i in range (3):
+        n=random.randint(0,2)
+        comp=possibilities[n].lower()
+        print("Rock"," paper ","Scissor")
+        user=input("Enter your choice:")
+        user=user.lower()
+        while user not in ["rock","paper","scissor"]:
+            print("Invalid input, please choose Rock, paper or Scissor")
+            user=input("Enter your choice:")
+            user=user.lower()
+        if comp==user:
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("Draw")
+        elif comp=="rock" and user=="scissor":
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("Computer wins")
+            win+=1
+        elif comp=="rock" and user=="paper":
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("User wins")
+            loss+=1
+        elif comp=="paper" and user=="rock":    
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("Computer wins")
+            win+=1
+        elif comp=="paper" and user=="scissor":
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("User wins")
+            loss+=1
+        elif comp=="scissor" and user=="rock":
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("User wins")
+            loss+=1
+        elif comp=="scissor" and user=="paper":
+            print("User's choice=",user.upper," Computer's choice=",comp)
+            print("Computer wins")
+            win+=1
+    if win>loss:
+        print("Sorry! the computer has won the game")
+    elif loss>win:
+        print("Congrats! You have won the game")
+    else:
+        print("The game is a draw") 
+    n=int(input("If you wanna play again enter any number other than 0:"))
